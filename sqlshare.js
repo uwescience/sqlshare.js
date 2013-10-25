43a065c6f3253680a1a182082df7be7c7743b8e0
Daniel Halperin <dhalperi@cs.washington.edu>
2013-10-24 18:50:43 -0700

    start of a demo.html file to test it out.
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- sqlshare.js
+++ sqlshare.js
@@ -1,3 +1,19 @@
 (function() {
-   var sqlshare_rest_server = "https://rest.sqlshare.escience.washington.edu";
+  sqlshare = {
+    version : "0.0.1",
+  }
+  var sqlshare_rest_server = "https://rest.sqlshare.escience.washington.edu";
+  sqlshare.rest_server = sqlshare_rest_server;
+
+  var sqlshare_all_datasets = function() {
+    var dataset_uri = sqlshare_rest_server + '/REST.svc/v2/db/dataset';
+    return d3.json(dataset_uri, function (json, error) {
+      if (error) {
+        console.log(error);
+	return;
+      }
+      return json;
+    });
+  };
+  sqlshare.all_datasets = sqlshare_all_datasets;
 })();
